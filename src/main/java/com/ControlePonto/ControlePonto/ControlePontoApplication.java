e5af02d4887c8022f12bdf5fb298fd9c6f1bb341
Daniel-Flores-S <danielfloresda802@gmail.com>
2022-09-09 22:26:32 -0300

    Added swagger

--- src/main/java/com/ControlePonto/ControlePonto/ControlePontoApplication.java
+++ src/main/java/com/ControlePonto/ControlePonto/ControlePontoApplication.java
@@ -2,8 +2,14 @@ package com.ControlePonto.ControlePonto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableCaching
+@EnableAsync
 public class ControlePontoApplication {
 
 	public static void main(String[] args) {
